55ba09db4c1a7c0010ec91f626cd4199ce609470
Sweekar Dahal <sweekardhl@gmail.com>
2025-11-27 14:56:32 +0530

    React learnong Starts

---
+++ react-folder/src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>hello world!</h1>
+      </div>
+      <div>
+        <h1>My name is Sweekar Dahal</h1>
+        <div>
+          <h2>This is a Div created inside nother div</h2>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
