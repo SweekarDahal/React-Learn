24f8ff331c2f23d6b1f1847be6f7d1699fa70d07
Sweekar Dahal <sweekardhl@gmail.com>
2026-01-11 13:34:53 +0530

    Learned basic React Router DOM, Types of Routers and Tried out BrowserRouter for single page website and its applications

---
+++ 18-react-router-dom/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import About from './About'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Welcome home</h1>
+    </div>
+  )
+}
+
+export default Home
